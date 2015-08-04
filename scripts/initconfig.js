f7e14d1d95e5c2e9f96db1f42d7119e5faf6da11
Mark Lapierre <mlapierre@gmail.com>
2015-08-04 14:02:43 -0400

    Fix api and couchdb urls so they're configurable

--- scripts/initconfig.js
+++ scripts/initconfig.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
 var config = fs.readFileSync('scripts/config.tmpl', 'utf8');
 
-config = config.replace(/%PORT%/g, process.env.API_PORT);
-config = config.replace(/%ADDR%/g, process.env.API_ADDR);
-config = config.replace(/%COUCHDB_ADDR%/g, process.env.COUCHDB_ADDR);
-config = config.replace(/%COUCHDB_PORT%/g, process.env.COUCHDB_PORT);
+config = config.replace(/%API_PORT%/g, process.env.API_PORT);
+config = config.replace(/%API_ADDR%/g, process.env.API_ADDR);
+config = config.replace(/%FEEDERDB_ADDR%/g, process.env.FEEDERDB_ADDR);
+config = config.replace(/%FEEDERDB_PORT%/g, process.env.FEEDERDB_PORT);
 
 fs.writeFile("app/js/config.js", config, function(err) {
     if(err) {
